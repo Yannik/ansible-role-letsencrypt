f1775deaa0fd7c313cc26992d1218c45312d945b
Yannik Sembritzki <yannik@sembritzki.me>
2016-12-18 20:54:31 +0100

    renew.sh should fail early

--- templates/renew.sh
+++ templates/renew.sh
@@ -1,4 +1,8 @@
-#!/bin/bash
+#!/usr/bin/env bash
+set -o errexit
+set -o pipefail
+set -o nounset
+
 cd {{ letsencrypt_dir }}/{{ item.name }}
 python ../acme-tiny/acme_tiny.py --account-key account.key --csr domain.csr --acme-dir {{ letsencrypt_challenge_dir }} > signed.crt
 cat signed.crt ../lets-encrypt-x3-cross-signed.pem > chained.crt
