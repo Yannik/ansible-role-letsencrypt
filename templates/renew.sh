da9850e2a0aca9d8908a84c803a2668a37720826
Yannik Sembritzki <yannik@sembritzki.me>
2018-01-04 03:53:14 +0100

    If no webserver is running, use temporary python server
    This fixes the problem, where the webserver cannot come up due to a missing cert,
    which stops the letsencrypt process, due to which the webserver cannot come up etc..

--- templates/renew.sh
+++ templates/renew.sh
@@ -4,8 +4,24 @@ set -o pipefail
 set -o nounset
 
 cd {{ letsencrypt_dir }}/{{ item.name }}
+
+# do nothing if certificate is valid for more than 30 days (30*24*60*60)
+[ -s signed.crt ] && openssl x509 -noout -in signed.crt -checkend 2592000 > /dev/null && exit
+
+if ! lsof -i:80 > /dev/null; then
+    mkdir -p serve/.well-known
+    ln -sf "{{ letsencrypt_challenge_dir }}" serve/.well-known/acme-challenge
+    cd serve
+    python3 -m http.server 80 > /dev/null &
+    cd ..
+fi
+
 python ../acme-tiny/acme_tiny.py --account-key account.key --csr domain.csr --acme-dir {{ letsencrypt_challenge_dir }} > signed.crt
 cat signed.crt ../lets-encrypt-x3-cross-signed.pem > chained.crt
+cat chained.crt domain.key > chained_cert+key.pem
+
+pkill -f -9 "python3 -m http.server 80" || true
+rm -rf serve
 
 if [ -s /etc/init.d/apache2 ]; then
     /etc/init.d/apache2 reload
