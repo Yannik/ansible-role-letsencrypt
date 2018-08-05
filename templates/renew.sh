b70661b6c7c57a077f38e504fe8e3786223ab579
Yannik Sembritzki <yannik@sembritzki.me>
2018-08-05 13:48:24 +0200

    Use dehydrated instead of acme-tiny to support dns challenges

--- templates/renew.sh
+++ templates/renew.sh
@@ -5,9 +5,6 @@ set -o nounset
 
 cd {{ letsencrypt_dir }}/{{ item.name }}
 
-# do nothing if certificate is valid for more than 31 days (31*24*60*60)
-[ -s signed.crt ] && openssl x509 -noout -in signed.crt -checkend 2678400 > /dev/null && exit
-
 if ! lsof -i:80 > /dev/null; then
     mkdir -p serve/.well-known
     ln -sf "{{ letsencrypt_challenge_dir }}" serve/.well-known/acme-challenge
@@ -16,7 +13,9 @@ if ! lsof -i:80 > /dev/null; then
     cd ..
 fi
 
-python ../acme-tiny/acme_tiny.py --account-key account.key --csr domain.csr --acme-dir {{ letsencrypt_challenge_dir }} > signed.crt
+../dehydrated/dehydrated --cron --config dehydrated.conf {% for subdomain in item.subdomains %}--domain {{ subdomain }} {% endfor %} --alias {{ item.name }}
+
+cat certs/{{ item.name }}/cert.pem > signed.crt
 cat signed.crt ../lets-encrypt-x3-cross-signed.pem > chained.crt
 cat chained.crt domain.key > chained_cert+key.pem
 
@@ -24,13 +23,13 @@ pkill -f -9 "python3 -m http.server 80" || true
 rm -rf serve
 
 if [ -s /etc/init.d/apache2 ]; then
-    ansible --connection=local -i localhost, -m service -a "name=apache2 state=reloaded" localhost > /dev/null
+    ansible --connection=local -i localhost, -m service -a "name=apache2 state=reloaded" localhost > /dev/null || true
 fi
 
 if [ -s /etc/init.d/dovecot ]; then
-    ansible --connection=local -i localhost, -m service -a "name=dovecot state=reloaded" localhost > /dev/null
+    ansible --connection=local -i localhost, -m service -a "name=dovecot state=reloaded" localhost > /dev/null || true
 fi
 
 if [ -s /etc/init.d/postfix ]; then
-    ansible --connection=local -i localhost, -m service -a "name=postfix state=reloaded" localhost > /dev/null
+    ansible --connection=local -i localhost, -m service -a "name=postfix state=reloaded" localhost > /dev/null || true
 fi
