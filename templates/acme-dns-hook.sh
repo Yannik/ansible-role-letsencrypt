b5108d99af1052029d1f05ce20e4daec5d105f45
Yannik Sembritzki <yannik@sembritzki.org>
2021-07-01 02:05:00 +0200

    Fix wildcard cert verification

--- templates/acme-dns-hook.sh
+++ templates/acme-dns-hook.sh
@@ -9,7 +9,7 @@ case "$ACTION" in
     "deploy_challenge")
 	case "$DOMAIN" in
 {% for subdomain in item.subdomains|selectattr('acme_domain_id', 'defined') %}
-            "{{ subdomain.name }}")
+            "{{ subdomain.name|regex_replace('^\\*\\.', '') }}")
                 result=$(curl --silent -X POST \
                   -H "X-Api-User: {{ subdomain.acme_user }}" \
                   -H "X-Api-Key: {{ subdomain.acme_pass }}" \
