Yannik/ansible-role-letsencrypt

commit 398498bb298cdc5f3ec7bfea00928b25d492e9f8

Use chain cert provided by dehydrated to combat problems upon changing intermediate certs

templates/renew.sh
@@ -46,7 +46,7 @@ fi
 
 cp certs/{{ item.name }}/cert.pem signed.crt
 cp certs/{{ item.name }}/privkey.pem domain.key
-cat signed.crt ../lets-encrypt-r3.pem > chained.crt
+cp certs/{{ item.name }}/fullchain.pem chained.crt
 cat chained.crt domain.key > chained_cert+key.pem
 
 if [ -s /etc/init.d/apache2 ]; then

tasks/letsencrypt.yml
@@ -29,11 +29,6 @@
 - name: Create acme-challenge dir
   file: path={{ letsencrypt_challenge_dir }} state=directory mode=o+x
 
-- name: Download letsencrypt intermediate
-  get_url:
-    url: https://letsencrypt.org/certs/lets-encrypt-r3.pem
-    dest: "{{ letsencrypt_dir }}/lets-encrypt-r3.pem"
-
 - name: Clone dehydrated
   git:
     repo: https://github.com/lukas2511/dehydrated.git

commit c2409529ce274d0f6768720e5c06344a2299e466

Fix incorect chained.crt file (regression introduced in 398498bb)

templates/renew.sh
@@ -42,7 +42,7 @@ fi
 
 cp certs/{{ item.name }}/cert.pem signed.crt
 cp certs/{{ item.name }}/privkey.pem domain.key
-cp certs/{{ item.name }}/fullchain.pem chained.crt
+cat signed.crt certs/{{ item.name }}/chain.pem > chained.crt
 cat chained.crt domain.key > chained_cert+key.pem
 
 {% if item.user is defined %}