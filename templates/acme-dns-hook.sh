aa3bdac6b4eb28e78e43a87274628f2b25349343
Yannik Sembritzki <yannik@sembritzki.org>
2022-11-30 11:44:46 +0100

    Allow setting acme-dns credentials per certificate instead of per subdomain

--- templates/acme-dns-hook.sh
+++ templates/acme-dns-hook.sh
@@ -21,9 +21,50 @@ case "$ACTION" in
 		fi
             ;;
 {% endfor %}
+{% if 'acme_domain_id' in item %}
+            *)
+                result=$(curl --silent -X POST \
+                  -H "X-Api-User: {{ item.acme_user }}" \
+                  -H "X-Api-Key: {{ item.acme_pass }}" \
+                  -d "{\"subdomain\": \"{{ item.acme_domain_id }}\", \"txt\": \"$TOKEN\"}" \
+                  $SERVER/update)
+		if echo $result | jq -e '.error' > /dev/null; then
+		    echo "Error: $result"
+		    exit 1
+		fi
+            ;;
+{% endif %}
         esac
         ;;
     "clean_challenge")
+	case "$DOMAIN" in
+{% for subdomain in item.subdomains|selectattr('acme_domain_id', 'defined') %}
+            "{{ subdomain.name|regex_replace('^\\*\\.', '') }}")
+                result=$(curl --silent -X POST \
+                  -H "X-Api-User: {{ subdomain.acme_user }}" \
+                  -H "X-Api-Key: {{ subdomain.acme_pass }}" \
+                  -d "{\"subdomain\": \"{{ subdomain.acme_domain_id }}\", \"txt\": \"$TOKEN\"}" \
+                  $SERVER/delete)
+		if echo $result | jq -e '.error' > /dev/null; then
+		    echo "Error: $result"
+		    exit 1
+		fi
+            ;;
+{% endfor %}
+{% if 'acme_domain_id' in item %}
+            *)
+                result=$(curl --silent -X POST \
+                  -H "X-Api-User: {{ item.acme_user }}" \
+                  -H "X-Api-Key: {{ item.acme_pass }}" \
+                  -d "{\"subdomain\": \"{{ item.acme_domain_id }}\", \"txt\": \"$TOKEN\"}" \
+                  $SERVER/delete)
+		if echo $result | jq -e '.error' > /dev/null; then
+		    echo "Error: $result"
+		    exit 1
+		fi
+            ;;
+{% endif %}
+        esac
         ;;
     "deploy_cert")
         # optional:
--- templates/multihook.sh
+++ templates/multihook.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-{% if item.subdomains|selectattr('acme_domain_id', 'defined')|list|length > 0 %}
+{% if item.subdomains|selectattr('acme_domain_id', 'defined')|list|length > 0 or 'acme_domain_id' in item %}
 ./acme-dns-hook.sh "$@"
 {% endif %}
 {% for hook in item.hooks|default([]) %}
